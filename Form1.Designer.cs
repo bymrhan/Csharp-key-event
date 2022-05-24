e132b3e502cad766febde9c7d87e1156ed96c0dc
Emrehan Baysal <81330668+bymrhan@users.noreply.github.com>
2022-05-24 16:30:53 +0300

    publish

---
+++ Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿
+namespace keyevent
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(84, 49);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(81, 17);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "Delete Aktif";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(270, 122);
+            this.Controls.Add(this.checkBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emrehanbysal;
+
+namespace keyevent
+{
+    public partial class Form1 : Form
+    {
+        globalKeyboardHook klavyeDinleyicisi = new globalKeyboardHook();
+        public Form1()
+        {
+         
+            InitializeComponent();
+            DinlenecekTuslariAyarla();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            checkBox1.Checked = true;
+        }
+        public void DinlenecekTuslariAyarla()
+        {
+            // hangi tuşları dinlemek istiyorsak burada ekliyoruz                
+            klavyeDinleyicisi.HookedKeys.Add(Keys.Delete);
+       
+
+            //basıldığında ilk burası çalışır
+            klavyeDinleyicisi.KeyDown += new KeyEventHandler(islem1);
+            //basıldıktan sonra ikinci olarak burası çalışır
+            klavyeDinleyicisi.KeyUp += new KeyEventHandler(islem2);
+        }
+        void islem1(object sender, KeyEventArgs e)
+        {         
+            
+            if(i != 1)
+            {
+                e.Handled = false;
+            }
+            else
+            {
+                e.Handled = true;
+            }
+          
+        }
+
+        void islem2(object sender, KeyEventArgs e)
+        {
+            e.Handled = true;
+           
+        }
+
+        int i = 0;
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if(checkBox1.Checked == true)
+            {
+                checkBox1.Text = "Delete Deactive";
+                i = 1;
+            }
+            else
+            {               
+                checkBox1.Text = "Delete active";
+                i = 0;
+            }
+        }
+
+        private void login_Click(object sender, EventArgs e)
+        {
+            checkBox1.Checked = true;
+         
+        
+
+        }
+    }
+}
